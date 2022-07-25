398f4398703b7dfccb759dde38a65630738e2084
Vu Van Tai <vutai13196@gmail.com>
2022-07-25 23:32:50 +0700

    fix cors done

--- src/server.js
+++ src/server.js
@@ -8,11 +8,11 @@ require('dotenv').config(); //giúp chạy process.env.PORT
 
 
 let app = express();
-// app.use(cors({ origin: true }));
+// app.use(cors({ origin: 'true' }));
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', process.env.POST_URL);
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
