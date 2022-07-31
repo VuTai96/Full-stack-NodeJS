d86ad7ab84140f12e4b3b1e961b4f9ed9a2beb49
Vu Van Tai <vutai13196@gmail.com>
2022-07-31 15:20:42 +0700

    edit rout login

--- src/services/userService.js
+++ src/services/userService.js
@@ -52,7 +52,7 @@ const handleUserLogin = (email, password) => {
         try {
             let data = {}
             const user = await db.User.findOne({
-                attributes: ['email', 'roleId', 'password'],
+                attributes: ['email', 'roleId', 'password', 'firstName', 'lastName'],
                 where: { email: email },
                 raw: true
             })
